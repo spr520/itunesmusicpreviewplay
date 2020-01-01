7934dc3385a6c4410c89f66050f057a4e504a710
spencer chen <spr520@gmail.com>
2020-01-01 13:22:18 +0800

    add searchViw

--- app/src/main/java/com/edoo/itunesmusicpreviewtrackplay/MainActivity.java
+++ app/src/main/java/com/edoo/itunesmusicpreviewtrackplay/MainActivity.java
@@ -1,12 +1,19 @@
 package com.edoo.itunesmusicpreviewtrackplay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 
 import com.edoo.itunesmusicpreviewtrackplay.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "SPR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }
+        getSupportActionBar().setTitle(R.string.action_bar_title);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.searchbar, menu);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
+            search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+            search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String text) {
+                    Log.d(TAG, "keyword = " + text);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String query) {
+                    return true;
+                }
+
+            });
+
+        }
+
+        return true;
     }
 }
