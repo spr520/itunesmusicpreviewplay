4d4204fe9f4ebd2062cc19cd281047c3fa2d5f29
spencer chen <spr520@gmail.com>
2020-01-01 12:21:12 +0800

    Initial commit

---
+++ app/src/main/java/com/edoo/itunesmusicpreviewtrackplay/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.edoo.itunesmusicpreviewtrackplay.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
